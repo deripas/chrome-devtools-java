52ad42e25b20c3a4778a63f544fc214de97701d8
deripas <deripas@yandex.ru>
2025-02-18 22:55:53 +0100

    fix

--- chrome-devtools-api-generator/src/test/java/io/github/deripas/chrome/protocol/ProtocolGeneratorExample.java
+++ chrome-devtools-api-generator/src/test/java/io/github/deripas/chrome/protocol/ProtocolGeneratorExample.java
@@ -9,7 +9,7 @@ public class ProtocolGeneratorExample {
     public static void main(String[] args) {
         new CommandLine(new ProtocolGeneratorCommand())
             .execute(
-                "-p", "org.deripas.chrome.protocol.api",
+                "-p", "io.github.deripas.chrome.devtools.api",
                 "-o", "chrome-devtools-api/src/main/generated-java",
                 "--dry-run",
                 "protocol/browser_protocol.json",
---
+++ chrome-devtools-api/src/main/generated-java/io/github/deripas/chrome/devtools/api/target/TargetInfo.java
@@ -0,0 +1,65 @@
+package io.github.deripas.chrome.devtools.api.target;
+
+import io.github.deripas.chrome.devtools.api.browser.BrowserContextID;
+import io.github.deripas.chrome.devtools.api.page.FrameId;
+import java.lang.Boolean;
+import java.lang.String;
+import javax.annotation.Nullable;
+import jdk.jfr.Experimental;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Generated;
+
+@Data
+@Builder(
+    toBuilder = true
+)
+@Generated
+public class TargetInfo {
+  private final TargetID targetId;
+
+  /**
+   * List of types: https://source.chromium.org/chromium/chromium/src/+/main:content/browser/devtools/devtools_agent_host_impl.cc?ss=chromium&q=f:devtools%20-f:out%20%22::kTypeTab%5B%5D%22
+   */
+  private final String type;
+
+  private final String title;
+
+  private final String url;
+
+  /**
+   * Whether the target has an attached client.
+   */
+  private final Boolean attached;
+
+  /**
+   * Opener target Id
+   */
+  @Nullable
+  private final TargetID openerId;
+
+  /**
+   * Whether the target has access to the originating window.
+   */
+  @Experimental
+  private final Boolean canAccessOpener;
+
+  /**
+   * Frame id of originating window (is only set if target has an opener).
+   */
+  @Nullable
+  @Experimental
+  private final FrameId openerFrameId;
+
+  @Nullable
+  @Experimental
+  private final BrowserContextID browserContextId;
+
+  /**
+   * Provides additional details for specific target types. For example, for
+   * the type of "page", this may be set to "prerender".
+   */
+  @Nullable
+  @Experimental
+  private final String subtype;
+}
